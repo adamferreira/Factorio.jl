d126c011b999a32aabe3a1b16db4c72e0cf8f63a
adamferreira <adam.ferreira.dc@gmail.com>
2022-10-26 15:55:23 +0000

    Fix io subpackage import

--- src/Factorio.jl
+++ src/Factorio.jl
@@ -3,13 +3,12 @@ using JSON, DataFrames
 
 DATA_DIR = joinpath(@__DIR__, "..", "data")
 
-include("IO/load.jl")
-
+include("io/load.jl")
 include("DataModel/DataModel.jl")
 
-m = DefaultFactorioDataBase()
-a = AssemblingMachine(3, m)
-@show sizeof(a)
+#m = DefaultFactorioDataBase()
+#a = AssemblingMachine(3, m)
+#@show sizeof(a)
 #@show consumption(Electricity, AssemblingMachines(), a)
 #@show consumption(Fuel, AssemblingMachines(), a)
 #@show tier(a)
