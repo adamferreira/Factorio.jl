0f6cb39cabdcf09fa344fc383397984372fa8d98
adamferreira <adam.ferreira.dc@gmail.com>
2023-05-29 22:20:09 +0200

    Load items to dataframe !

--- src/DataModel/NewDataModel.jl
+++ src/DataModel/NewDataModel.jl
@@ -3,20 +3,25 @@ using JSON
 
 DATA_DIR = joinpath(@__DIR__, "..", "..", "data")
 
-function load_items()
+function load_items()::DataFrame
     d = JSON.parsefile(joinpath(DATA_DIR, "item.json"))
+    cols = ["name", "type", "stack_size", "fuel_value"]
+    df = DataFrame([(c => []) for c in cols]...)
     for (name, desc) in d
-
-        println(name)
+        push!(df, [desc[c] for c in cols])
     end
+    return df
 end
 
-function load_items2()
-    d = JSON.parsefile(joinpath(DATA_DIR, "recipe.json"))
+function load_fluids()::DataFrame
+    d = JSON.parsefile(joinpath(DATA_DIR, "fluid.json"))
     df = DataFrame()
     map(x -> append!(df, d[x]), [name for (name, desc) in d])
     return df
 end
 
-#load_items()
-print(load_items2())
+items = load_items()
+#print(load_fluids())
+#d = load_fluids()
+#@show d[1, :]
+fuel = filter(row -> row.fuel_value > 0, items)
